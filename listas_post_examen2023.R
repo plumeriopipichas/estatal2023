e77ae1e72f692789c244b0ce07cc20519993ad34
plumeriopipichas <plumeriopipichas@gmail.com>
2023-04-04 08:09:57 -0600

    Add files via upload

--- listas_post_examen2023.R
+++ listas_post_examen2023.R
@@ -15,8 +15,8 @@ sedes <- substr(dir(path),14,m-4)
 for (sede in sedes){
   x<-grep(sede,dir(path))
   basica_sedes_oficial[[sede]]<-read.csv(
-    paste(path,dir(path)[x],sep=""))
-  print(dim(basica_sedes_oficial[[sede]]))
+    paste(path,dir(path)[x],sep=""))%>%
+    select(-sede)
 }
 
 lista_general_participacion<-juntar_bases(basica_sedes_oficial)
@@ -66,10 +66,8 @@ lista_general_participacion <-rbind(lista_general_participacion,
           read.csv("../listas_crudas/modificados_y_extras.csv"))
 print(c("despues",nrow(lista_general_participacion)))
 
-#-------------un poco de limpiez a la lista general ----------
+#-------------un poco de limpieza a la lista general ----------
 
-x<-which(lista_general_participacion$Nombre=="")
-lista_general_participacion <- lista_general_participacion[-x, ]
 lista_general_participacion$Primer_apellido<-
   limpiar(lista_general_participacion$Primer_apellido)
 lista_general_participacion$Segundo_apellido<-
@@ -77,30 +75,29 @@ lista_general_participacion$Segundo_apellido<-
 lista_general_participacion$Nombre<-
   limpiar(lista_general_participacion$Nombre)
 
-
 #---------- para las constancias de entrenadores ----------
 
-entrenadores_sedes <-list()
-path<-"../listas_crudas/entrenadores"
-m<-nchar(dir(path))
-sedes <- substr(dir(path),14,m-4)
-for (sede in sedes){
-  x<-grep(sede,dir(path))
-  entrenadores_sedes[[sede]]<-read.csv(
-    paste(path,"/",dir(path)[x],sep=""))
-  entrenadores_sedes[[sede]]$sede<-sede
-  print(c("entrenadores",sede))
-  print(dim(entrenadores_sedes[[sede]]))
-}
-
-lista_entrenadores<-juntar_bases(entrenadores_sedes)
-lista_entrenadores$Institucion<-subte(lista_entrenadores$Institucion)
-lista_entrenadores$Nombre<-notitle(lista_entrenadores$Nombre)
-lista_entrenadores$Nombre<-limpiar(lista_entrenadores$Nombre)
-lista_entrenadores<-unique(lista_entrenadores)
-
-write.csv(lista_entrenadores,"../listas_generadas/lista_entrenadores.csv",row.names = FALSE)
-rm(entrenadores_sedes)
+# entrenadores_sedes <-list()
+# path<-"../listas_crudas/entrenadores"
+# m<-nchar(dir(path))
+# sedes <- substr(dir(path),14,m-4)
+# for (sede in sedes){
+#   x<-grep(sede,dir(path))
+#   entrenadores_sedes[[sede]]<-read.csv(
+#     paste(path,"/",dir(path)[x],sep=""))
+#   entrenadores_sedes[[sede]]$sede<-sede
+#   print(c("entrenadores",sede))
+#   print(dim(entrenadores_sedes[[sede]]))
+# }
+# 
+# lista_entrenadores<-juntar_bases(entrenadores_sedes)
+# lista_entrenadores$Institucion<-subte(lista_entrenadores$Institucion)
+# lista_entrenadores$Nombre<-notitle(lista_entrenadores$Nombre)
+# lista_entrenadores$Nombre<-limpiar(lista_entrenadores$Nombre)
+# lista_entrenadores<-unique(lista_entrenadores)
+# 
+# write.csv(lista_entrenadores,"../listas_generadas/lista_entrenadores.csv",row.names = FALSE)
+#rm(entrenadores_sedes)
 
 #----------Depurar listas con las respuestas
 
@@ -117,8 +114,7 @@ for (i in 1:12){
   vars<-c(vars,temp)
 }
 
-for (sede in sedes[c(1:8,11:15)]){
-  print(c("jojo",sede))
+for (sede in sedes){
   path <- paste("../listas_crudas/respuestas_examen/",
                 sede,"2023.csv", sep="")
   respuestas_sedes[[sede]]<-read.csv(path)%>%
@@ -131,16 +127,105 @@ for (sede in sedes[c(1:8,11:15)]){
              lista_general_participacion$clave)
   if (length(x)<nrow(respuestas_sedes[[sede]])){
     sin_registro[[sede]]<-respuestas_sedes[[sede]][-x, ]
-    sin_registro[[sede]]$sede<-sede
+    sin_registro[[sede]]$Sede<-sede
   }
 }
 
+
 sin_registro_todos <- juntar_bases(sin_registro)
 respuestas_examen <- juntar_bases(respuestas_sedes)
+posterior<-read.csv("../listas_crudas/respuestas_examen/posterior.csv")%>%
+  select(all_of(vars))
 
+
+respuestas_examen <- rbind(respuestas_examen,posterior)
+                           
 
 write.csv(sin_registro_todos,"../listas_generadas/
           examenes_sin_registro.csv",row.names = FALSE)
 
+#-----------Juntar las puntuaciones con los datos personales y hacer
+# listas asicociadas: comite examen,constancias de participacion --------
 
-rm(i,m,nr,path,sede,temp,x)
+
+for (k in 1:nrow(respuestas_examen)){
+  if (respuestas_examen$Aciertos[k]>67){
+    respuestas_examen$Aciertos[k]<-respuestas_examen$Aciertos[k]-68
+  }
+}
+
+x <- which(names(respuestas_examen)=="Identificacion")
+names(respuestas_examen)[x] <- "clave"
+
+lista_completa <- merge(lista_general_participacion,respuestas_examen,
+                        by ="clave",all.y=TRUE)%>%
+                  select(-numero_sede)
+          
+lista_completa$nacio_en <- as.integer(
+  paste("20",substr(lista_completa$CURP,5,6),sep=""))
+
+comite_examen <- select(lista_completa,clave,nacio_en,nivel,Nombre,
+                             Aciertos)%>%
+                      filter(!is.na(Nombre))%>%
+                      arrange(desc(Aciertos),Nombre)
+x<-which(comite_examen$nacio_en<2003)
+comite_examen$nacio_en[x]<-""
+x<-grep("ecun",comite_examen$nivel)
+y<-grep("ille",comite_examen$nivel)
+comite_examen_secundarias<-arrange(comite_examen[x, ],desc(Aciertos))
+comite_examen_prepas<-arrange(comite_examen[y, ],desc(Aciertos))
+
+para_constancias <- select(lista_completa,Nombre,Primer_apellido,Segundo_apellido,Escuela,Correo)%>%
+  filter(!is.na(Correo))
+  
+para_constancias$Escuela<-subte(para_constancias$Escuela)
+
+# write.csv(comite_examen,"../listas_generadas/puntuaciones_primer_examen/
+#           puntos_primer_examen_2023.csv",row.names=FALSE)
+# write.csv(comite_examen_secundarias,"../listas_generadas/puntuaciones_primer_examen/
+#           puntos_primer_examen_sec2023.csv",row.names=FALSE)
+# write.csv(comite_examen_prepas,"../listas_generadas/puntuaciones_primer_examen/
+#           puntos_primer_examen_bach2023.csv",row.names=FALSE)
+
+write.csv(para_constancias,"../listas_generadas/lista_constancias_participacion.csv")
+
+#---------- lista para publicar quienes pasan a segunda etapa -------
+
+segunda_etapa_sec <- filter(lista_completa,Aciertos>7,!is.na(Nombre),
+                            nivel=="Secundaria")%>%
+                      select(Nombre,Primer_apellido,Segundo_apellido,Escuela,
+                             nivel)
+
+segunda_etapa_ms <- filter(lista_completa,Aciertos>8,!is.na(Nombre),
+                            nivel=="Bachillerato")%>%
+  select(Nombre,Primer_apellido,Segundo_apellido,Escuela,nivel)
+
+segunda_etapa<-rbind(segunda_etapa_sec,segunda_etapa_ms)%>%
+  arrange(Nombre,Primer_apellido,Segundo_apellido)%>%
+  mutate(Nombre_completo=paste(Nombre,Primer_apellido,Segundo_apellido))%>%
+  select(Nombre_completo,Escuela,nivel)
+
+
+
+
+rm(segunda_etapa_ms,segunda_etapa_sec)
+
+write.csv(segunda_etapa,"../listas_generadas/publicar_segunda_etapa.csv")
+
+#----------para sacar puntuaciones por escuela
+
+revision_escuelas <-select(lista_completa,-c(CURP,Sede,nivel,clave))%>%
+  filter(!is.na(Clave_escuela))
+
+revision_escuelas$Clave_escuela<-trimws(revision_escuelas$Clave_escuela,which = "both")
+x<-which(revision_escuelas$Clave_escuela=="")
+revision_escuelas<-revision_escuelas[-x, ]
+revision_escuelas<-group_by(revision_escuelas,Clave_escuela)
+
+revisar_escuelas<-summarise(revision_escuelas,cuantos=n(),suma_puntos=sum(Aciertos))%>%
+  arrange(desc(suma_puntos))
+
+
+revision_escuelas <-ungroup(revision_escuelas)
+
+rm(i,k,m,nr,path,sede,temp,vars,x,y)
